95fab43bd2159166e8895aa461b022a6709a6adc
François Frisch <francoisfrisch@gmail.com>
2014-04-14 11:58:20 -0700

    Make more names more descriptive.

--- ui/facadeflow.reel/facadeflow.js
+++ ui/facadeflow.reel/facadeflow.js
@@ -1,96 +1,24 @@
 
 var Component   = require("montage/ui/component").Component;
 
-//TODO rename to Movie List
+//TODO rename to Filmstrip
 exports.Facadeflow = Component.specialize({
     constructor: {
         value: function Facadeflow () {
-            this.defineBinding("selectedMovie", {"<-": "categoriesController.selection.one()"});
-            this.application.addEventListener( "initialDataReady", this, false);
+            this.super();
         }
     },
 
-    selectedMovie: {
-        value: null
-    },
-
-    movieFlowDidTranslateStart: {
-        value: function () {
-            this.detailsFadeOut = true;
-            this.detailsFadeIn = false;
-            this.needsDraw = true;
-        }
-    },
-
-    movieFlowDidTranslateEnd: {
-        value: function (flow) {
-            var scroll = Math.round(flow.scroll);
-            if(this._contentController) {
-                this._contentController.select(this._contentController.content[scroll]);
-            }
-            this.detailsFadeIn = true;
-            this.detailsFadeOut = false;
-            this.needsDraw = true;
-        }
-    },
-
-
-    _fadeIn: {
-        value: false
-    },
-
-    _fadeOut: {
-        value: false
-    },
-
-    latestBoxOffice: {
-        value: null
-    },
-
-    upcomingMovies: {
-        value: null
-    },
-
-    inTheaters: {
-        value: null
-    },
-
-    topDvdRentals: {
-        value: null
-    },
-
-    category: {
-        value: null
-    },
-
-    _contentController: {
-        value: null
-    },
-
-    _switchValue: {
-        value: null
-    },
-
-    switchValue: {
-        set: function (val) {
-            this._switchValue = val;
-        },
-        get: function () {
-            return this._switchValue;
-        }
-    },
-
-    _categoryId: {
-        value: null
-    },
-    categoryId: {
-        get: function () {
-            return this._categoryId;
-        },
-        set: function (value) {
-            if (value) {
-                this._categoryId = value;
-                this._changeCategory(value);
+    enterDocument: {
+        value: function (firstTime) {
+            if (firstTime) {
+                this.movieDetails.defineBinding(
+                    "classList.has('details-fade-out')",
+                    {
+                        "<-": "$self._detailsHidden",
+                        parameters: { self: this }
+                    }
+                );
             }
         }
     },
@@ -105,73 +33,63 @@ exports.Facadeflow = Component.specialize({
         },
         set: function (value) {
             if (value == null) { return; }
+            this._categoryContentController = value;
             if (this._categoryContentController == null) {
                 // first time
-                this._categoryContentController = value;
-                this._contentController = value;
-                this.detailsFadeIn = true;
-                this._fadeIn = true;
-                this.needsDraw = true;
+                this._displayedContentController = this._categoryContentController;
+                this._flowHidden = false;
             } else {
-                this._categoryContentController = value;
-                //TODO change _changeCategory() name
-                this._changeCategory();
+                this._startChangeCategoryTransition();
             }
         }
     },
 
-    _changeCategory: {
+    _displayedContentController: {
+        value: null
+    },
+
+    _startChangeCategoryTransition: {
         value: function () {
             var self = this;
 
-            this.detailsFadeOut = true;
-            this._fadeOut = true;
-            this.needsDraw = true;
-            // wait .5s until the fade out effect is completed
+            this._detailsHidden = true;
+            this._flowHidden = true;
+
+            // wait .5s until the fade in/out effect is completed
             setTimeout( function () {
-                if (self.templateObjects && self.templateObjects.flow) {
+                // reset the flow to initial scroll position
+                if (self.templateObjects && self.templateObjects.movieFlow) {
                     self.templateObjects.movieFlow.scroll = 0;
                 }
-                //TODO rename this _contentController isn't descriptive
-                self._contentController = self.categoryContentController;
-                self._fadeIn = true;
-                self._fadeOut = false;
-                self.detailsFadeIn = true;
-                self.detailsFadeOut = false;
-                self.needsDraw = true;
-
+                self._displayedContentController = self.categoryContentController;
+                self._flowHidden = false;
+                self._detailsHidden = false;
             }, 800 );
         }
     },
 
-    draw: {
+    movieFlowDidTranslateStart: {
         value: function () {
-            var flow = this.templateObjects.movieFlow,
-                details = this.details;
-
-            if( this._fadeIn ){
-                flow.element.classList.remove( 'flow-fade-out');
-                this._fadeIn = false;
-            }
-
-            if( this._fadeOut ){
-                flow.element.classList.add( 'flow-fade-out');
-                this._fadeOut = false;
-            }
-
-            if( this.detailsFadeIn ){
-                details.element.classList.remove('details-fade-out');
-                this.detailsFadeIn = false;
-            }
-
-            if( this.detailsFadeOut ){
-                if( details.element.classList.contains('details-fade-out') === false ){
-                    details.element.classList.add('details-fade-out');
-                }
-                this.detailsFadeOut = false;
-            }
-
+            this._detailsHidden = true;
         }
+    },
+
+    movieFlowDidTranslateEnd: {
+        value: function (flow) {
+            var scroll = Math.round(flow.scroll);
+            if(this._displayedContentController) {
+                this._displayedContentController.select(this._displayedContentController.content[scroll]);
+            }
+            this._detailsHidden = false;
+        }
+    },
+
+    _flowHidden: {
+        value: false
+    },
+
+    _detailsHidden: {
+        value: false
     }
 
 });
