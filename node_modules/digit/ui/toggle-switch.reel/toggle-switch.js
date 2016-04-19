2dc578885907ba0122940a712a7916383a85b1a8
pixelsight <noreply>
2016-04-19 12:07:28 +0000

    auto-recovery

--- node_modules/digit/ui/toggle-switch.reel/toggle-switch.js
+++
@@ -1,4 +0,0 @@
-var Montage = require("montage/core/core").Montage,
-    AbstractToggleSwitch = require("montage/ui/base/abstract-toggle-switch").AbstractToggleSwitch;
-
-exports.ToggleSwitch = AbstractToggleSwitch.specialize({ });
