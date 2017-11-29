01ac432de25cb90f7bdb9b8961160ee3aef41c3a
Harold Thetiot <hthetiot@gmail.com>
2017-11-28 17:17:19 -0800

    fix reload iframe on resize

--- ui/player.reel/player.js
+++ ui/player.reel/player.js
@@ -1,7 +1,7 @@
 
 var Component = require("montage/ui/component").Component;
 
-var TRAILER_URL = "https://www.youtube.com/embed/%s?autoplay=1&enablej‌​sapi=1",
+var TRAILER_URL = "https://www.youtube.com/embed/%s?autoplay=1&enablejsapi=1",
     PLACE_HOLDER = "%s";
 
 exports.Player = Component.specialize({
@@ -38,7 +38,7 @@ exports.Player = Component.specialize({
     didShowOverlay: {
         value: function (overlay) {
             if (this._trailerId) {
-                var trailerUrl = TRAILER_URL.replace(PLACE_HOLDER, this._trailerId);
+                var trailerUrl = TRAILER_URL.replace(PLACE_HOLDER, encodeURIComponent(this._trailerId));
                 if (this.player.src !== trailerUrl) {
                     this.player.src = trailerUrl;
                 }
