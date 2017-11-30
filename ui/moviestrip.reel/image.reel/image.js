1d11d42617082533dc26c814838d9f59c111210d
Harold Thetiot <hthetiot@gmail.com>
2017-11-29 20:48:07 -0800

    fix poster missing title

--- ui/moviestrip.reel/image.reel/image.js
+++ ui/moviestrip.reel/image.reel/image.js
@@ -6,6 +6,10 @@ exports.Image = Component.specialize( {
         value: null
     },
 
+    _title: {
+        value: null
+    },
+
     src: {
         set: function (value) {
             if (value !== this._src) {
@@ -15,8 +19,19 @@ exports.Image = Component.specialize( {
         }
     },
 
+    title: {
+        set: function (value) {
+            if (value !== this._title) {
+                this._title = value;
+                this.needsDraw = true;
+            }
+        }
+    },
+
     draw: {
         value: function () {
+            this._element.setAttribute('title', this._title);
+            
             if (this._src) {
                 this._element.style.backgroundImage = "url(" + this._src + ")";
             } else {
