f0df33a94fa2183265a62afb8caa9b77fda00a65
Harold Thetiot <hthetiot@gmail.com>
2018-02-14 18:08:37 -0800

    add ?lang= support

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -18,10 +18,27 @@ exports.Main = Component.specialize({
             });
             */
 
+            var localeParam = this.getParameterByName('lang');
+            if (localeParam) {
+                defaultLocalizer.locale = localeParam;
+            }
+
             this.application.addEventListener( "openTrailer", this, false);
 
             this.canDrawGate.setField("moviesLoaded", false);
             this._initialDataLoad = this.moviesService.load();
+        }
+    },
+
+    getParameterByName: {
+            value: function (name, url) {
+            if (!url) url = window.location.href;
+            name = name.replace(/[\[\]]/g, "\\$&");
+            var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+                results = regex.exec(url);
+            if (!results) return null;
+            if (!results[2]) return '';
+            return decodeURIComponent(results[2].replace(/\+/g, " "));
         }
     },
 
