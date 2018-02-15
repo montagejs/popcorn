3be47eccd704cc866b4f64f6f58aa4fa55da6846
Harold Thetiot <hthetiot@gmail.com>
2018-02-14 17:41:18 -0800

    add loader while poster load and display no-poster on error

--- ui/moviestrip.reel/image.reel/image.js
+++ ui/moviestrip.reel/image.reel/image.js
@@ -1,6 +1,7 @@
 var Component = require("montage/ui/component").Component;
+var Promise = require('montage/core/promise').Promise;
 
-exports.Image = Component.specialize( {
+exports.Image = Component.specialize({
 
     _src: {
         value: null
@@ -28,15 +29,48 @@ exports.Image = Component.specialize( {
         }
     },
 
+    _loadImage: {
+        value: (function () {
+            var images = {};
+            return function (url) {
+                if (!images.hasOwnProperty(url)) {
+                    var defer = Promise.defer();
+                    var img = new Image();
+                    img.onload = function () {
+                        defer.resolve(url);
+                    };
+                    img.onerror = function () {
+                        defer.reject(url);
+                    };
+                    img.src = url;
+                    images[url] = defer.promise;
+                }
+                return images[url];
+            }
+        }()),
+    },
+
     draw: {
         value: function () {
-            this._element.setAttribute('title', this._label);
-            this._element.setAttribute('alt', this._label);
-            
-            if (this._src) {
-                this._element.style.backgroundImage = "url(" + this._src + ")";
+
+            var self = this,
+                element = self._element;
+
+            element.setAttribute('title', self._label);
+            element.setAttribute('alt', self._label);
+
+            if (self._src) {
+                element.style.backgroundImage = "url(assets/image/loading-poster.gif)";
+                element.style.backgroundSize = 'auto';
+                self._loadImage(self._src).then(function () {
+                    element.style.backgroundImage = "url(" + self._src + ")";
+                }).catch(function () {
+                    element.style.backgroundImage = "url(assets/image/no-poster.jpg)";
+                }).finally(function () {
+                    element.style.backgroundSize = '';
+                });
             } else {
-                this._element.style.backgroundImage = "url(assets/image/no-poster.jpg)";
+                element.style.backgroundImage = "url(assets/image/no-poster.jpg)";
             }
         }
     }
