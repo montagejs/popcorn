311ab8d9b8fb8fa95f0e58980845cb9b784abae7
Harold Thetiot <hthetiot@gmail.com>
2018-04-06 11:21:42 -0700

    fix ./bin/push-test.js

--- bin/push-test.js
+++ bin/push-test.js
@@ -1,17 +1,23 @@
+#!/usr/bin/env node
+
 const webpush = require('web-push');
 // https://console.firebase.google.com
-const SERVER_API_KEY="";
+//const SERVER_API_KEY="";
 // From push-worker subscription
-const pushSubscription = {"endpoint":"...","expirationTime":null,"keys":{"p256dh":"..."}};
+//const pushSubscription = {"endpoint":"...","expirationTime":null,"keys":{"p256dh":"..."}};
 
 webpush.setGCMAPIKey(SERVER_API_KEY);
 webpush.sendNotification(pushSubscription, JSON.stringify({
 	tag: Date.now(),
 	method:"push",
 	title: "Montage Popcorn",
+    icon: '/assets/icons/icon-196.png',
+    badge:'/assets/icons/icon-128.png',
+    sound: '/assets/audio/pop_up_alert.mp3',
 	body: "There's a new trailer!"
 })).then(function (res) {
 	console.log('ok', res)
 }).catch(function (err) {
 	console.error('err', err)
 });
+	
